72147d53048528ab5ec405ad29762644615e0150
BijonTalukder <bijontalukder1247@gmail.com>
2023-06-12 11:25:28 +0600

    set preitter husky eslint lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-
-const app:Application = express()
+const app: Application = express()
 const port = 3000
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.get('/', (req:Request, res:Response) => {
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
