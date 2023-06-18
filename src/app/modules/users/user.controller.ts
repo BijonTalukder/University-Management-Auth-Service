e5b88a9b5dd530598337de797390746db2f69d85
BijonTalukder <bijontalukder1247@gmail.com>
2023-06-18 10:31:21 +0600

    A

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,15 +1,12 @@
-import { Request, Response } from "express";
-import usersService from "./users.service";
+import { Request, Response } from 'express'
+import usersService from './users.service'
 
-exports.createUser = async (req:Request,res:Response)=>{
-    try{
+exports.createUser = async (req: Request, res: Response) => {
+  try {
+    const result = await usersService.createUser(req.body)
 
-        const result = await usersService.createUser(req.body)
-
-res.status(200).json({sucess:true,message:'succrss ',data:result})
-    }
-    catch(e){
-        res.status(400).json({sucess:false,message:'failed to '})
-
-    }
+    res.status(200).json({ sucess: true, message: 'succrss ', data: result })
+  } catch (e) {
+    res.status(400).json({ sucess: false, message: 'failed to ' })
+  }
 }
