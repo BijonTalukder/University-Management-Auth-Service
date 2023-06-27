35e08a27dcde0a10c8bcd269c0d53e4c4b483a19
BijonTalukder <81519797+BijonTalukder@users.noreply.github.com>
2023-06-28 02:21:43 +0600

    academic semester service done

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,21 +1,50 @@
-import { RequestHandler } from "express";
-import { AcademicSemesterService } from "./academicSemester.service";
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import { AcademicSemesterService } from './academicSemester.service'
+import pick from '../../../shared/pick'
 
+const createSemester: RequestHandler = async (req, res, next) => {
+  try {
+    const { ...academicSemesterData } = req.body
+    const result = await AcademicSemesterService.createSemester(
+      academicSemesterData
+    )
+    res.status(200).json({
+      success: true,
+      message: 'Academic semester is created successfully!',
+      data: result,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
 
-const createSemester:RequestHandler = async (req,res,next)=>{
-    try{
-const {...academicSemesterData} = req.body;
-const result = await AcademicSemesterService.createSemester(academicSemesterData)
-res.status(200).json({
-    success:true,
-    message:'Academic semester is created successfully!',
-    data:result
-})
-    }
-    catch(err){
-        next(err)
-    }
+const getAllSemester = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // const paginationOptions = {
+    //   page: Number(req.query.paage),
+    //   limit: Number(req.query.limit),
+    //   sortBy: req.query.sortBy,
+    //   sortOrder: req.query.sortOrder,
+    // }
+    const paginationOptions = pick(req.query,['page', 'limit', 'sortBy','sortOrder'])
+    const result = await AcademicSemesterService.getAllSemesters(paginationOptions)
+    res.status(200).json({
+      statusCode: 200,
+      success:true,
+      message:'semester retrive success',
+      data:result
+    })
+   
+  
+  } catch (err) {
+    next(err)
+  }
 }
 export const AcademicSemesterController = {
-    createSemester
+  createSemester,
+  getAllSemester,
 }
